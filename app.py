30d9afe9be78970e90f1df6b724600b0c83ef0e2
Chakri <geya.nitturu@gmail.com>
2026-02-12 21:48:36 +0530

    Phase 1: Implemented MediaPipe pose detection with live Streamlit webcam

---
+++ app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import cv2
+import mediapipe as mp
+import numpy as np
+
+st.title("PostureFit - Live Pose Detection")
+
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+
+run = st.checkbox("Start Camera")
+
+FRAME_WINDOW = st.image([])
+
+if run:
+    cap = cv2.VideoCapture(0)
+
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    ) as pose:
+
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                st.write("Camera not working")
+                break
+
+            # Convert BGR to RGB
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # Improve performance-Make only Readable
+            image.flags.writeable = False
+
+            # Process image
+            results = pose.process(image)
+
+            # Draw landmarks
+            # Make it Writable again
+            image.flags.writeable = True
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if results.pose_landmarks:
+                mp_drawing.draw_landmarks(
+                    image,
+                    results.pose_landmarks,
+                    mp_pose.POSE_CONNECTIONS
+                )
+
+            FRAME_WINDOW.image(image)
+
+    cap.release()
+
